a8a06ce42e251b7e5ab3fbd1a46c22a3b25d5406
ul1s3s07 <apereiraa2010@gmail.com>
2024-05-09 18:07:07 -0300

    first upload

---
+++ ejercicio_1.java
@@ -0,0 +1,9 @@
+public class ejercicio_1 {
+    /* Ejercicio_1: Un alumno tiene “n” notas (Se ingresa por teclado la cantidad de notas) que se deben
+cargar en un vector (Se debe definir el vector en base a la cantidad de notas que se van a
+ingresar, por ejemplo: Si son 5 notas, el vector debe ser de tamaño 5). Luego de cargar las
+notas se debe mostrar la nota más alta y el promedio de notas.*/
+
+        
+
+}
---
+++ ejercicio_4.java
@@ -0,0 +1,3 @@
+public class ejercicio_4 {
+    
+}
